f86235e2b5e395a6a2549ea1fe9609daab1bbf16
n.yoshida <inazuma074@gmail.com>
2019-07-28 02:44:10 +0900

    link outputFile to Database

--- src/article/ArticlesDAO.java
+++ src/article/ArticlesDAO.java
@@ -12,16 +12,27 @@ public class ArticlesDAO {
 	// db接続共通メソッド
 	public Connection DBConnection() {
 		Connection conn = null;
+		String DB_HOST = null;
+		String DB_DRIVER = null;
+
+	    String osname = System.getProperty("os.name");
+	    if(osname.indexOf("Windows")>=0) {
+	        DB_HOST = "jdbc:mariadb://192.168.179.7";
+	        DB_DRIVER = "org.mariadb.jdbc.Driver";
+	    } else if(osname.indexOf("Linux")>=0) {
+	        DB_HOST = "jdbc:mysql://localhost";
+	        DB_DRIVER = "com.mysql.jdbc.Driver";
+	    }
 
 		try {
-			Class.forName(System.getenv("DB_DRIVER"));
+			Class.forName(DB_DRIVER);
 		} catch(ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 
 		try {
 			conn = DriverManager.getConnection(
-				System.getenv("DB_HOST") + "/blog_sys",
+				DB_HOST + "/blog_sys",
 				"tomcat",
 				"2Bbbbbb\""
 			);
--- src/outputFile.java
+++ src/outputFile.java
@@ -8,9 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import article.ArticlesDAO;
+
 public class outputFile extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
+
+            ArticlesDAO articlesdao = new ArticlesDAO();
+
             response.setContentType("text/html; charaset=UTF-8");
             request.setCharacterEncoding("UTF-8");
 
@@ -32,6 +37,7 @@ public class outputFile extends HttpServlet {
                 File file = new File(filename);
                 file.createNewFile();
                 FileWriter filewriter = new FileWriter(file);
+                articlesdao.insertArticle(1, "title", filename);
 
                 filewriter.write(item1);
 
