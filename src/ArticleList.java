4a6e6c20033ae1fd3da299e52a08627de26a51db
n.yoshida <inazuma074@gmail.com>
2019-07-28 10:52:09 +0900

    add articleList page

---
+++ src/ArticleList.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import article.ArticlesDAO;
+import article.ArticlesDTO;
+
+public class ArticleList extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException{
+            request.setCharacterEncoding("utf-8");
+            response.setCharacterEncoding("utf-8");
+
+            ArticlesDAO articlesdao = new ArticlesDAO();
+            List<ArticlesDTO> articles = articlesdao.findAll();
+
+            PrintWriter out = response.getWriter();
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>記事一覧</title>");
+            out.println("</head>");
+            out.println("<body>");
+
+            for(int n = 0; n <= articles.size(); n++) {
+                out.println("<p>");
+                out.println(articles.get(n).getTitle());
+                out.println("</p>");
+            }
+
+            out.println("</body>");
+            out.println("</html>");
+        }
+}
--- src/article/ArticleMain.java
+++
@@ -1,8 +0,0 @@
-package article;
-
-public class ArticleMain {
-	public static void main(String[] args) {
-		ArticlesDAO dao = new ArticlesDAO();
-		dao.insertArticle(1, "test5", "file");
-	}
-}
