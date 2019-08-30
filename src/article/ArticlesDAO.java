5f4dec128146f9080874e6e0591b93d905be58f8
n.yoshida <inazuma074@gmail.com>
2019-08-31 00:37:03 +0900

    reset

--- src/article/ArticlesDAO.java
+++ src/article/ArticlesDAO.java
@@ -20,10 +20,8 @@ public class ArticlesDAO {
 	        DB_HOST = "jdbc:mariadb://192.168.179.7";
 	        DB_DRIVER = "org.mariadb.jdbc.Driver";
 	    } else if(osname.indexOf("Linux")>=0) {
-	        DB_HOST = "jdbc:mariadb://192.168.179.7";
-	        DB_DRIVER = "org.mariadb.jdbc.Driver";
-//	        DB_HOST = "jdbc:mysql://localhost";
-//	        DB_DRIVER = "com.mysql.jdbc.Driver";
+	        DB_HOST = "jdbc:mysql://localhost";
+	        DB_DRIVER = "com.mysql.jdbc.Driver";
 	    }
 
 		try {
