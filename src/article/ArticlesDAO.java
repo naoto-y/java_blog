130faccd81f122be690da3f23875d5292e1f39d8
n.yoshida <inazuma074@gmail.com>
2019-08-31 18:17:42 +0900

    test

--- src/article/ArticlesDAO.java
+++ src/article/ArticlesDAO.java
@@ -60,6 +60,7 @@ public class ArticlesDAO {
 				dto.setTitle(rset.getString("title"));
 				dto.setArticle_path(rset.getString("article_path"));
 //				dto.setUpload(rset.getDate("upload"));    //こいつが犯人
+				rset.getDate("upload");
 				dto.setModify(rset.getDate("modify"));
 				articlesDTO.add(dto);
 			}
