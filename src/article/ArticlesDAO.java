b4c3fce8f08d9391699173d947c1e27c119d5230
n.yoshida <inazuma074@gmail.com>
2019-09-16 15:23:37 +0900

    add commit process

--- src/article/ArticlesDAO.java
+++ src/article/ArticlesDAO.java
@@ -139,9 +139,10 @@ public class ArticlesDAO {
 	public void insertArticle(int user_id, String title, String path) {
 		try(Connection conn = this.DBConnection()) {
 			Statement stmt = conn.createStatement();
-//			String sql = "INSERT INTO article_list VALUES (null," + user_id + ",0,'" + title + "','" + path + "',0,now(),now())";
-			String sql = "INSERT INTO article_list VALUES (null,1,0,'title','path',0)";
+			String sql = "INSERT INTO article_list VALUES (null," + user_id + ",0,'" + title + "','" + path + "',0,now(),now())";
 			stmt.executeQuery(sql);
+			stmt.close();
+			conn.commit();
 		} catch(SQLException e) {
 			e.printStackTrace();
 		}
