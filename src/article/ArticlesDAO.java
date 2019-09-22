15c4683bff98a1529ff53ad7f5790045b86d6633
n.yoshida <inazuma074@gmail.com>
2019-09-23 03:12:35 +0900

    add commentClass

--- src/article/ArticlesDAO.java
+++ src/article/ArticlesDAO.java
@@ -1,53 +1,21 @@
 package article;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import database.Connecter;
+
 public class ArticlesDAO {
-	// db接続共通メソッド
-	public Connection DBConnection() {
-		Connection conn = null;
-		String DB_HOST = null;
-		String DB_DRIVER = null;
-
-	    String osname = System.getProperty("os.name");
-	    if(osname.indexOf("Windows")>=0) {
-	        DB_HOST = "jdbc:mariadb://192.168.179.7";
-	        DB_DRIVER = "org.mariadb.jdbc.Driver";
-	    } else if(osname.indexOf("Linux")>=0) {
-	        DB_HOST = "jdbc:mysql://localhost";
-	        DB_DRIVER = "com.mysql.jdbc.Driver";
-	    }
-
-		try {
-			Class.forName(DB_DRIVER);
-		} catch(ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			conn = DriverManager.getConnection(
-				DB_HOST + "/blog_sys",
-				"tomcat",
-				"2Bbbbbb\""
-			);
-		} catch(SQLException e) {
-			e.printStackTrace();
-		}
-
-		return conn;
-	}
 
 	public List<ArticlesDTO> findAll() {
 		List<ArticlesDTO> articlesDTO = new ArrayList<>();	//戻り値の変数宣言
 
 		// sql発行
-		try(Connection conn = this.DBConnection()) {
+		try(Connection conn = Connecter.DBConnection()) {
 			Statement stmt = conn.createStatement();
 			String sql = "SELECT * FROM article_list";
 			ResultSet rset = stmt.executeQuery(sql);
@@ -74,7 +42,7 @@ public class ArticlesDAO {
 	public int countAll() {
 	    int count = 0;
 
-	    try(Connection conn = this.DBConnection()){
+	    try(Connection conn = Connecter.DBConnection()){
 	        Statement stmt = conn.createStatement();
 	        String sql = "SELECT * FROM article_list";
 	        ResultSet rset = stmt.executeQuery(sql);
@@ -92,7 +60,7 @@ public class ArticlesDAO {
 	public ArticlesDTO findById(int id) {
 		ArticlesDTO articlesDTO = new ArticlesDTO();
 
-		try(Connection conn = this.DBConnection()) {
+		try(Connection conn = Connecter.DBConnection()) {
 			Statement stmt = conn.createStatement();
 			String sql = "SELECT * FROM article_list WHERE id = " + id;
 			ResultSet rset = stmt.executeQuery(sql);
@@ -114,7 +82,7 @@ public class ArticlesDAO {
 	public List<ArticlesDTO> findByUserId(int userId) {
 		List<ArticlesDTO> articlesDTO = new ArrayList<>();
 
-		try(Connection conn = this.DBConnection()) {
+		try(Connection conn = Connecter.DBConnection()) {
 			Statement stmt = conn.createStatement();
 			String sql = "SELECT * FROM article_list WHERE user_id = " + userId;
 			ResultSet rset = stmt.executeQuery(sql);
@@ -138,7 +106,7 @@ public class ArticlesDAO {
 
 	public void insertArticle(int user_id, String title, String path) {
 		try {
-		    Connection conn = this.DBConnection();
+		    Connection conn = Connecter.DBConnection();
 			Statement stmt = conn.createStatement();
 			String sql = "INSERT INTO article_list VALUES (null," + user_id + ",0,'" + title + "','" + path + "',0,now(),now())";
 			stmt.executeUpdate(sql); //insert句の場合はexecuteUpdateを使用しないといけない
